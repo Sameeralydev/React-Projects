c43277c2e324165148e37ecb3d3a41030d5efe3c
Sameer <sameeraly2003@gmail.com>
2025-05-20 18:44:27 +0500

    20-05

---
+++ cc/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { InputBox } from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+import './App.css'
+
+function App() {
+
+  return (
+   <h1 className='text-black bg-sky-500'>Currency Counter</h1>
+  )
+}
+
+export default App
